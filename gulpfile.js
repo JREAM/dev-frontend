256045a4fbc6c7512bf217246d9a3ae6d097c743
Jesse Boyer <aniyishay@gmail.com>
2014-08-19 23:19:30 -0400

    Oopsfixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-var gulp   = require('gulp')
-    coffee = require('gulp-coffee');
+var gulp    = require('gulp')
+    coffee  = require('gulp-coffee'),
+    watch   = require('gulp-watch'),
+    plumber = require('gulp-plumber');
 
 gulp.task('watch', function () {
     gulp.src('coffee/*.coffee')
